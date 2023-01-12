4e88c41470b61d21cd3ffe415243fc12e3c15455
Seb <S_Eisenspan@web.de>
2023-01-12 11:50:10 +0100

    test file access

--- funcs.js
+++ funcs.js
@@ -2,9 +2,18 @@ var yep = true;
 
 function buttonClick() {
 	if(yep){
-		document.getElementById("img1").src = "NOP.png"
+		document.getElementById("img1").src = "NOP.png";
 	} else {
-		document.getElementById("img1").src = "YEP.png"
+		document.getElementById("img1").src = "YEP.png";
 	}
 	yep = !yep;
+}
+
+function textFile() {
+	$.ajax({
+        type: "GET",
+        url: "text.txt",
+        dataType: "text",
+        success: function(data) {document.getElementById("a").innerHTML = data;}
+     });
 }
